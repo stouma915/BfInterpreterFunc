76018c9defb1eda5154d56656f3c25e4249b0dd5
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-29 12:37:49 +0900

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,53 +5,70 @@ import qualified Data.Map.Strict as M
 evaluate :: String -> Maybe String
 evaluate sourceCode = do
   let emptyMemory = M.fromList []
+  let emptyState = State 0 emptyMemory 0 "" False
 
-  eval sourceCode 0 emptyMemory 0 "" False
+  eval sourceCode emptyState
   where
-    eval :: String -> Int -> M.Map Int Int -> Int -> String -> Bool -> Maybe String
-    eval code index memory pointer output error = do
-      if error then
-        Nothing
-      else if index >= length code then
-        Just output
+    data State = State {
+      index :: Int,
+      memory :: M.Map Int Int,
+      pointer :: Int,
+      output :: String,
+      hasError :: Bool
+    }
+
+    eval :: String -> State -> State
+    eval code state = do
+      let ind = index state
+
+      if ind >= length code then
+        state
       else do
-        let newIndex = index + 1
+        let newInd = ind + 1
 
-        case code !! index of
-          '>' ->
-            eval code newIndex memory (pointer + 1) output error
-          '<' -> do
-            let newPointer = if pointer >= 1 then pointer - 1 else pointer
-            eval code newIndex memory newPointer output error
-          '+' ->
-            if not $ M.member pointer memory then
-              eval code newIndex (M.insert pointer 1 memory) pointer output error
-            else do
-              let current = memory M.! pointer
+        let mem = memory state
+        let ptr = pointer state
+        let out = output state
+        let err = hasError state
 
-              if current >= 255 then
-                eval code newIndex (M.update (\_ -> Just 0) pointer memory) pointer output error
-              else
-                eval code newIndex (M.update (\x -> Just (x + 1)) pointer memory) pointer output error
-          '-' ->
-            if not $ M.member pointer memory then
-              eval code newIndex (M.insert pointer 255 memory) pointer output error
-            else do
-              let current = memory M.! pointer
+        let newState = case code !! ind of
+            '>' ->
+              State newInd mem (ptr + 1) out err
+            '<' -> do
+              let newPtr = if ptr >= 1 then ptr - 1 else ptr
 
-              if current <= 0 then
-                eval code newIndex (M.update (\_ -> Just 255) pointer memory) pointer output error
-              else
-                eval code newIndex (M.update (\x -> Just (x - 1)) pointer memory) pointer output error
-          '.' ->
-            if not $ M.member pointer memory then
-              eval code newIndex memory pointer (output ++ "\0") error
-            else do
-              let current = memory M.! pointer
+              State newInd mem newPtr out err
+            '+' ->
+              if not $ M.member ptr mem then
+                State newInd (M.insert ptr 1 mem) ptr out err
+              else do
+                let current = mem M.! ptr
 
-              eval code newIndex memory pointer (output ++ [toEnum current :: Char]) error
-          _ ->
-            eval code newIndex memory pointer output error
+                if current >= 255 then
+                  State newInd (M.update (\_ -> Just 0) ptr mem) ptr out err
+                else
+                  State newInd (M.update (\x -> Just (x + 1)) ptr mem) ptr out err
+            '-' ->
+              if not $ M.member ptr mem then
+                State newInd (M.insert ptr 255 mem) ptr out err
+              else do
+                let current = mem M.! ptr
+
+                if current <= 0 then
+                  State newInd (M.update (\_ -> Just 255) ptr mem) ptr out err
+                else
+                  State newInd (M.update (\x -> Just (x - 1)) ptr mem) ptr out err
+            '.' ->
+              if not $ M.member ptr mem then
+                State newInd mem ptr (out ++ "\0") err
+              else do
+                let current = mem M.! ptr
+
+                State newInd mem ptr (out ++ [toEnum current :: Char]) err
+            _ ->
+              State newInd mem ptr out err
+        
+        eval code newState
 
     searchLoopEnd :: String -> Int -> Maybe Int
     searchLoopEnd code startIndex = search code startIndex 0 0
@@ -63,12 +80,12 @@ evaluate sourceCode = do
           else if index >= length src then
             Nothing
           else do
-            let newIndex = index + 1
+            let newInd = index + 1
 
             case src !! index of
               '[' ->
-                search src newIndex (x + 1) y
+                search src newInd (x + 1) y
               ']' ->
-                search src newIndex x (y + 1)
+                search src newInd x (y + 1)
               _ ->
-                search src newIndex x y
+                search src newInd x y
