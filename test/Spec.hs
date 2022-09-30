833512b825ec419c9ee9059630f67f9d1af71db3
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-30 09:33:32 +0900

    Update Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lib.evaluate" $ do
+    it "evaluates code and return output" $ do
+      evaluate "----[---->+<]>++.+.+.+." `shouldBe` Just("ABCD")
