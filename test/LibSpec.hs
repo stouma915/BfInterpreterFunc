1d5a60824db1521d7e8bf359b69b3533ce2ba57a
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-30 10:14:24 +0900

    Update LibSpec.hs

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -7,5 +7,6 @@ import Lib
 spec :: Spec
 spec = do
   describe "Lib.evaluate" $
-    it "evaluates code and return output" $
+    it "evaluates code and return output" $ do
       evaluate "----[---->+<]>++.+.+.+." `shouldBe` Just("ABCD")
+      evaluate "-[----->+<]>--.+.+.+.+.+.------." `shouldBe` Just("123456")
