4ea269ed5ee7506e581131cdfac26384492ad463
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-30 10:35:35 +0900

    Update LibSpec.hs

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -10,3 +10,5 @@ spec = do
     it "evaluates code and return output" $ do
       evaluate "----[---->+<]>++.+.+.+." `shouldBe` Just("ABCD")
       evaluate "-[----->+<]>--.+.+.+.+.+.------." `shouldBe` Just("1234560")
+      evaluate "-[------->+<]>-.[--->++++<]>+.--[--->+<]>--.--------.------.+++++++.." `shouldBe` Just("Haskell")
+      evaluate "[" `shouldBe` Nothing
